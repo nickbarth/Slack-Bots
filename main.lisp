e2ce5418e522986e8eec7f478ee1996ee2792d76
Nick Barth <nickbarth@live.com>
2016-08-28 22:59:44 -0600

    Update main.lisp

--- main.lisp
+++ main.lisp
@@ -27,30 +27,30 @@
             (cdr (assoc ':CHANNEL payload))
             (cdr (assoc ':TEXT payload)))))
 
-(defun post-slack-msg (slack msg channel)
+(defun post-slack-message (slack message channel)
   (https-json-request (concatenate 'string SLACK_MSG_URL
-    "&username=" (cdr (assoc ':NAME slack)) "&channel=" channel "&text=" msg)))
+    "&username=" (cdr (assoc ':NAME slack)) "&channel=" channel "&text=" message)))
 
 (defun get-card-image (card)
   (let ((api (concatenate 'string HS_API_URL (cl-ppcre:regex-replace-all " " card "%20")))
       (headers (list (cons "x-mashape-key" HS_API_KEY))))
     (cdr (assoc ':IMG (car (https-json-request api headers))))))
 
-(defun asked-for-card? (slack type msg)
+(defun asked-for-card? (slack type message)
   (and (string-equal type "message") 
-  (search (concatenate 'string "<@" (cdr (assoc ':ID slack)) "> card ") msg)))
+  (search (concatenate 'string "<@" (cdr (assoc ':ID slack)) "> card ") message)))
 
 (defun send-card-image (slack card channel)
-  (post-slack-msg slack (get-card-image card) channel))
+  (post-slack-message slack (get-card-image card) channel))
 
 (defun main ()
   (let* ((slack (get-slack-info))
         (client (make-client slack)))
     (wsd:on :message client
       (lambda (data)
-        (multiple-value-bind (ptype pchannel pmsg) (parse-slack-data data)
-          (if  (asked-for-card? slack ptype pmsg)
-            (send-card-image slack (subseq pmsg 18) pchannel)))))
+        (multiple-value-bind (type channel message) (parse-slack-data data)
+          (if  (asked-for-card? slack type message)
+            (send-card-image slack (subseq message 18) channel)))))
     (as:with-event-loop ()
       (wsd:start-connection client))))
 
