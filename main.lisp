26789055a4a77e6dbf5d0cf0dbd0f9360b7e761f
Nick Barth <nickbarth@live.com>
2016-08-29 01:12:53 -0600

    Update main.lisp

--- main.lisp
+++ main.lisp
@@ -1,7 +1,7 @@
 (ql:quickload '(:drakma :cl-json :websocket-driver-client :cl-ppcre) :silent t)
 
-(defconstant SLACK_API_KEY "xxxx")
-(defconstant HS_API_KEY "xxxx")
+(defconstant SLACK_API_KEY "XXXX")
+(defconstant HS_API_KEY "XXXX")
 
 (defvar *slack-rtm-url* (concatenate 'string "https://slack.com/api/rtm.start?token=" SLACK_API_KEY))
 (defvar *slack-msg-url* (concatenate 'string "https://slack.com/api/chat.postMessage?as_user=true&token=" SLACK_API_KEY))
@@ -12,14 +12,10 @@
         (drakma:http-request url :additional-headers headers :parameters params))))
     (json:decode-json-from-string resp)))
 
-(defun make-client (slack)
-  (wsd:make-client (cdr (assoc ':URL slack))))
-
 (defun get-slack-info ()
   (let ((json (https-json-request *slack-rtm-url*)))
-    (list (assoc ':ID (cdr (assoc ':SELF json)))
-          (assoc ':NAME (cdr (assoc ':SELF json)))
-          (assoc ':URL json))))
+    (values (cdr (assoc ':ID (cdr (assoc ':SELF json))))
+            (cdr (assoc ':URL json)))))
 
 (defun parse-slack-data (data)
   (let ((payload (json:decode-json-from-string data)))
@@ -27,9 +23,9 @@
             (cdr (assoc ':CHANNEL payload))
             (cdr (assoc ':TEXT payload)))))
 
-(defun send-slack-message (slack message channel)
+(defun send-slack-message (message channel)
   (https-json-request *slack-msg-url* nil
-    (list (cons "username" (cdr (assoc ':NAME slack))) (cons "channel" channel) (cons "text" message))))
+    (list (cons "channel" channel) (cons "text" message))))
 
 (defun get-card-image (card)
   (let* ((api (concatenate 'string *hs-api-url* (cl-ppcre:regex-replace-all " " card "%20")))
@@ -38,22 +34,22 @@
     (if (eq (car json) ':ERROR)
       "Card Not Found" (cdr (assoc ':IMG json)))))
 
-(defun asked-for-card? (slack type message)
+(defun asked-for-card? (user-id type message)
   (and (string-equal type "message")
-  (search (concatenate 'string "<@" (cdr (assoc ':ID slack)) "> card ") message)))
+  (search (concatenate 'string "<@" user-id "> card ") message)))
 
-(defun send-card-image (slack card channel)
-  (send-slack-message slack (get-card-image card) channel))
+(defun send-card-image (card channel)
+  (send-slack-message (get-card-image card) channel))
 
 (defun main ()
-  (let* ((slack (get-slack-info))
-        (client (make-client slack)))
-    (wsd:on :message client
-      (lambda (data)
-        (multiple-value-bind (type channel message) (parse-slack-data data)
-          (if (asked-for-card? slack type message)
-            (send-card-image slack (subseq message 18) channel)))))
-    (as:with-event-loop ()
-      (wsd:start-connection client))))
+  (multiple-value-bind (user-id ws-url) (get-slack-info)
+    (let ((client (wsd:make-client ws-url)))
+      (wsd:on :message client
+        (lambda (data)
+          (multiple-value-bind (type channel message) (parse-slack-data data)
+            (if (asked-for-card? user-id type message)
+              (send-card-image (subseq message 18) channel)))))
+      (as:with-event-loop ()
+        (wsd:start-connection client)))))
 
 (main)
