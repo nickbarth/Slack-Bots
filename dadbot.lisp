b90d70da733e82545ebe9cdc63f0f50f0773c69d
Nick Barth <nickbarth@live.com>
2018-09-20 12:01:08 -0600

    Update dadbot.lisp

--- dadbot.lisp
+++ dadbot.lisp
@@ -1,10 +1,9 @@
-(ql:quickload '(:drakma :cl-json :websocket-driver-client) :silent t)
-
-(defconstant SLACK_API_KEY "XXXX")
+(ql:quickload '(:drakma :cl-async :cl-json :websocket-driver-client) :silent t)
 
 (setf *random-state* (make-random-state t))
+
+(defconstant SLACK_API_KEY "XXXX")
 (defvar *slack-rtm-url* (concatenate 'string "https://slack.com/api/rtm.start?token=" SLACK_API_KEY))
-(defvar *slack-msg-url* (concatenate 'string "https://slack.com/api/chat.postMessage?as_user=true&token=" SLACK_API_KEY))
 
 (defun https-json-request (url &optional headers params)
   (let* ((resp (flexi-streams:octets-to-string
@@ -82,40 +81,38 @@
   "Dad, did you get a haircut? No I got them all cut."
   "Have you heard of the band 1023MB? They haven't got a gig yet."))
 
-(defun get-joke () 
+(defun get-joke ()
  (nth (random (length *jokes*)) *jokes*))
 
 (defun get-slack-info ()
   (let ((json (https-json-request *slack-rtm-url*)))
-    (values (cdr (assoc ':ID (cdr (assoc ':SELF json))))
-            (cdr (assoc ':URL json)))))
+    (cdr (assoc ':URL json))))
 
 (defun parse-slack-data (data)
   (let ((payload (json:decode-json-from-string data)))
-    (values (cdr (assoc ':TYPE payload))
-            (cdr (assoc ':CHANNEL payload))
-            (cdr (assoc ':TEXT payload)))))
-
-(defun send-slack-message (message channel)
-  (https-json-request *slack-msg-url* nil
-    (list (cons "channel" channel) (cons "text" message))))
+    (list (cdr (assoc ':TYPE payload))
+          (cdr (assoc ':CHANNEL payload))
+          (cdr (assoc ':TEXT payload)))))
 
 (defun was-dad-mentioned? (type message)
   (and (string-equal type "message")
        (search "dad" message)))
 
+(defvar *ws-url* (get-slack-info))
+(defvar *client* (wsd:make-client *ws-url*))
+
+(defun send-slack-message (message channel)
+  (wsd:send *client* (json:encode-json-to-string 
+    `(("type" . "message") ("text" . ,message) ("channel" . ,channel)))))
+
 (defun send-joke (channel)
   (send-slack-message (get-joke) channel))
 
-(defun main ()
-  (multiple-value-bind (user-id ws-url) (get-slack-info)
-    (let ((client (wsd:make-client ws-url)))
-      (wsd:on :message client
-        (lambda (data)
-          (multiple-value-bind (type channel message) (parse-slack-data data)
-            (if (was-dad-mentioned? type message)
-              (send-joke channel)))))
-      (as:with-event-loop ()
-        (wsd:start-connection client)))))
+(defun receive-data (data)
+  (destructuring-bind (type channel message) (parse-slack-data data)
+    (if (was-dad-mentioned? type message) (send-joke channel))))
 
-(main)
+(wsd:on :message *client* #'receive-data)
+
+(as:with-event-loop (:catch-app-errors t)
+  (wsd:start-connection *client*))
